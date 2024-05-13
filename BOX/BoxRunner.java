249a7cfc228541b8dda4f9dbc352fb446456fa2a
kspier5952 <159089040+kspier5952@users.noreply.github.com>
2024-05-13 15:00:12 -0400

    did some

---
+++ BOX/BoxRunner.java
@@ -0,0 +1,8 @@
+package BOX;
+
+public class BoxRunner {
+    public static void main(String[]args){
+        Box boxOne = new Box(2, 6);
+        System.out.println(boxOne.getArea());
+    }
+}
---
+++ CIRCLE/Circle.java
@@ -0,0 +1,5 @@
+package CIRCLE;
+
+public class Circle {
+    
+}
