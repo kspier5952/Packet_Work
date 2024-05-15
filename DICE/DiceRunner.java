ddee730b13643a4edb7ea894e0fdc17aa2d5f72d
kspier5952 <159089040+kspier5952@users.noreply.github.com>
2024-05-14 21:59:52 -0400

    Update DiceRunner.java

---
+++ DICE/DiceRunner.java
@@ -0,0 +1,12 @@
+public class DiceRunner {
+    public static void main(String[]args) {
+        Dice d = new Dice();
+        d.roll();
+        int n = d.getTotal();
+        System.out.println(n);
+        
+        d.roll();
+        n = d.getTotal();
+        System.out.println(n);
+    }
+}
